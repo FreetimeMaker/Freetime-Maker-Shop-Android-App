3b9cd3d6add76607539ac8aa3d2a4e3d1b94e067
FreetimeMaker <jamie.achatz@s-stb.ch>
2025-10-13 09:50:13 +0000

    added more

--- app/src/main/java/com/freetime/shop/ui/feature/home/HomeScreen.kt
+++ app/src/main/java/com/freetime/shop/ui/feature/home/HomeScreen.kt
@@ -1,17 +1,20 @@
 package com.freetime.shop.ui.feature.home
 
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.navigation.NavController
 import org.koin.androidx.compose.koinViewModel
+import com.freetime.domain.model.Product
 
 @Composable
 fun HomeScreen(navController: NavController, viewModel: HomeViewModel = koinViewModel()) {
-    val uiState = viewModel.uiState.collectAsState().value
+    val uiState = viewModel.uiState.collectAsState()
 
-    when (uiState) {
+    when (uiState.value) {
         is HomeScreenUIEvents.Loading -> {
             CircularProgressIndicator()
         }
@@ -19,8 +22,17 @@ fun HomeScreen(navController: NavController, viewModel: HomeViewModel = koinView
         is HomeScreenUIEvents.Success -> {
             val data = (uiState as HomeScreenUIEvents.Success).data
             LazyColumn {
-
+                items(data) { product ->
+                    ProductItem(product = product)
+                }
             }
+        }
+
+        is HomeScreenUIEvents.Error -> {
+            Text(text = (uiState as HomeScreenUIEvents.Error).message)
         }
     }
 }
+
+@Composable
+fun ProductItem(product: Product)
